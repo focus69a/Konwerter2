40270d81e0e00e234765b76576fac651d8711c27
focus69a <40357086+focus69a@users.noreply.github.com>
2018-09-24 11:50:56 +0200

    Initial commit

---
+++ app/src/main/java/com/wj/pl33111/konwerter2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.wj.pl33111.konwerter2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonCovnMilesToKm = (Button) findViewById(R.id.buttonMiles2Km);
+        buttonCovnMilesToKm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textBoxMiles = (EditText) findViewById(R.id.editTextMiles);
+                EditText textBoxKm = (EditText) findViewById(R.id.editTextKm);
+                double vMiles = Double.valueOf(textBoxMiles.getText().toString());
+                double vKm = vMiles / 0.62137;
+
+                DecimalFormat formatVal = new DecimalFormat("##.##");
+                textBoxKm.setText(formatVal.format(vKm));
+            }
+                                               });
+
+        Button buttonCovnKmToMiles = (Button) findViewById(R.id.buttonKm2Miles);
+        buttonCovnKmToMiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            EditText textBoxMiles = (EditText) findViewById(R.id.editTextMiles);
+            EditText textBoxKm = (EditText) findViewById(R.id.editTextKm);
+            double vKm = Double.valueOf(textBoxKm.getText().toString());
+            double vMiles = vKm * 0.62137;
+
+            DecimalFormat formatVal = new DecimalFormat("##.##");
+            textBoxMiles.setText(formatVal.format(vMiles));
+                   }
+
+              });
+    }
+}

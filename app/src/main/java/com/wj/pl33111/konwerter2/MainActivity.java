9713e9ef9ceeaf491d8578e5154b00a08d7e3b97
focus69a <40357086+focus69a@users.noreply.github.com>
2018-09-24 12:39:10 +0200

    b
    b

--- app/src/main/java/com/wj/pl33111/konwerter2/MainActivity.java
+++ app/src/main/java/com/wj/pl33111/konwerter2/MainActivity.java
@@ -8,8 +8,12 @@ import android.widget.EditText;
 
 import java.text.DecimalFormat;
 <<<<<<< HEAD
+<<<<<<< HEAD
 //testowy tekst powinie znalezc sie w branch 1
 //dodatkowy tekst do branch 1
+=======
+//testowy tekst
+>>>>>>> parent of 13628cf... zapis tekstu testowego do branch 1
 =======
 //testowy tekst
 >>>>>>> parent of 13628cf... zapis tekstu testowego do branch 1
